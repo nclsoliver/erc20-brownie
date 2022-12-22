405ffe6d3b1bcf02f4fe6f6c8314066548dc0d44
nclsoliver <96521061+nclsoliver@users.noreply.github.com>
2022-12-22 13:50:52 +0100

    building the contract

--- contracts/OurToken.sol
+++ contracts/OurToken.sol
@@ -3,9 +3,12 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./IERC20.sol";
+import "./extensions/IERC20Metadata.sol";
+import "../../utils/Context.sol";
 
-contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
+contract OurToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
         _mint(msg.sender, initialSupply);
     }
 }
